f973f528718833cad4267f8e6d8ba92928ca8485
Agni Patel <77357349+patelagni21@users.noreply.github.com>
2022-09-30 01:48:18 -0400

    Time: 113 ms (45.15%), Space: 14.1 MB (41.30%) - LeetHub

---
+++ 1470-shuffle-the-array/1470-shuffle-the-array.py
@@ -0,0 +1,14 @@
+class Solution:
+        #two pointers approach
+        def shuffle(self, nums: List[int], n: int) -> List[int]:
+                left = 0 
+                right = len(nums) // 2
+                result = []
+                
+                while left < len(nums) // 2 and right < len(nums):
+                        result.append(nums[left])
+                        left += 1
+                        
+                        result.append(nums[right])
+                        right += 1            
+                return result
